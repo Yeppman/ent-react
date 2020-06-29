c1662b1304098a07e7a09b7392ee922e569a80a8
Yeppman <40640334+Yeppman@users.noreply.github.com>
2020-06-29 05:03:36 +0100

    updated imports

--- src/components/features/TwoColWithSteps.js
+++ src/components/features/TwoColWithSteps.js
@@ -2,8 +2,8 @@ import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
-import TeamIllustrationSrc from "images/team-illustration-2.svg";
+import { SectionHeading, Subheading as SubheadingBase } from "../../components/misc/Headings.js";
+import TeamIllustrationSrc from "../../images/team-illustration-2.svg";
 import { ReactComponent as SvgDotPattern } from "images/dot-pattern.svg";
 
 const Container = tw.div`relative`;
--- src/demos/LandingPage.js
+++ src/demos/LandingPage.js
@@ -1,22 +1,22 @@
 import React from "react";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
-import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import Hero from "components/hero/TwoColumnWithInput.js";
-import Features from "components/features/ThreeColWithSideImage.js";
-import MainFeature from "components/features/TwoColWithButton.js";
-import MainFeature2 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
-import FeatureWithSteps from "components/features/TwoColWithSteps.js";
+import AnimationRevealPage from "../helpers/AnimationRevealPage.js";
+import Hero from "../components/hero/TwoColumnWithInput.js";
+import Features from "../components/features/ThreeColWithSideImage.js";
+import MainFeature from "../components/features/TwoColWithButton.js";
+import MainFeature2 from "../components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+import FeatureWithSteps from "../components/features/TwoColWithSteps.js";
 // import Pricing from "components/pricing/ThreePlans.js";
-import Pricing from "components/pricing/ThreePlansWithHalfPrimaryBackground.js";
+import Pricing from "../components/pricing/ThreePlansWithHalfPrimaryBackground.js";
 
-import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
-import FAQ from "components/faqs/SingleCol.js";
-import GetStarted from "components/cta/GetStarted";
-import Footer from "components/footers/FiveColumnDark.js";
-import heroScreenshotImageSrc from "images/hero-screenshot-1.png";
-import macHeroScreenshotImageSrc from "images/hero-screenshot-2.png";
-import prototypeIllustrationImageSrc from "images/prototype-illustration.svg";
+import Testimonial from "../components/testimonials/TwoColumnWithImageAndRating.js";
+// import FAQ from "components/faqs/SingleCol.js";
+import GetStarted from "../components/cta/GetStarted";
+import Footer from "../components/footers/FiveColumnDark.js";
+import heroScreenshotImageSrc from "../images/hero-screenshot-1.png";
+import macHeroScreenshotImageSrc from "../images/hero-screenshot-2.png";
+import prototypeIllustrationImageSrc from "../images/prototype-illustration.svg";
 import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
 import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
 

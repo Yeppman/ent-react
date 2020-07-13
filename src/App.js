70f425ac067917cb31429cced39fd463585c3ee5
Yeppman <40640334+Yeppman@users.noreply.github.com>
2020-07-13 17:33:03 +0100

    amrr

--- src/App.js
+++ src/App.js
@@ -21,6 +21,19 @@ import './assets/css/sidebar.css'
 import './App.css'
 import './assets/css/antd-override.css'
 
+import './assets/css/cart.css'
+
+import './assets/styles/Navigation.less'
+
+import './assets/excite_style/dashboard.css'
+import './assets/excite_style/home.css'
+import './assets/excite_style/nav.css'
+
+import CustomLayout from "./layout"
+//import CustomLayout from "./excite_components/containers/layout"
+import PageRouter from './routes'
+
+import './assets/excite_style/shop.css'
 import CustomLayout from "./layout"
 //import CustomLayout from "./excite_components/containers/layout"
 import PageRouter from './routes'
